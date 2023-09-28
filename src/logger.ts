29ad6f5def0aff1d7e11564e99035171191e3f30
freearhey <7253922+freearhey@users.noreply.github.com>
2023-09-29 00:15:34 +0300

    Update logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,16 @@
-import signale, { SignaleOptions } from 'signale'
+import { Signale, SignaleOptions } from 'signale'
 import treeify from 'object-treeify'
 
-const { Signale } = signale
+export class Logger {
+  logger: Signale
 
-export class Logger extends Signale {
   constructor(options?: SignaleOptions) {
-    super(options)
+    this.logger = new Signale(options)
+    this.logger.config({
+      displayLabel: false,
+      displayScope: false,
+      displayBadge: false
+    })
   }
 
   tree(object: object) {
@@ -16,5 +21,69 @@ export class Logger extends Signale {
     })
 
     this.info(output)
+  }
+
+  await(...args: any) {
+    this.logger.await(...args)
+  }
+
+  complete(...args: any) {
+    this.logger.complete(...args)
+  }
+
+  debug(...args: any) {
+    this.logger.debug(...args)
+  }
+
+  fatal(...args: any) {
+    this.logger.fatal(...args)
+  }
+
+  fav(...args: any) {
+    this.logger.fav(...args)
+  }
+
+  note(...args: any) {
+    this.logger.note(...args)
+  }
+
+  pause(...args: any) {
+    this.logger.pause(...args)
+  }
+
+  pending(...args: any) {
+    this.logger.pending(...args)
+  }
+
+  star(...args: any) {
+    this.logger.star(...args)
+  }
+
+  warn(...args: any) {
+    this.logger.warn(...args)
+  }
+
+  watch(...args: any) {
+    this.logger.watch(...args)
+  }
+
+  log(...args: any) {
+    this.logger.log(...args)
+  }
+
+  info(...args: any) {
+    this.logger.info(...args)
+  }
+
+  error(...args: any) {
+    this.logger.error(...args)
+  }
+
+  start(...args: any) {
+    this.logger.start(...args)
+  }
+
+  success(...args: any) {
+    this.logger.success(...args)
   }
 }
