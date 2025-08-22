3d0f5996b8fc379e72768626f0b5ec196b921920
freearhey <7253922+freearhey@users.noreply.github.com>
2025-08-22 21:56:54 +0300

    Update zip.ts

--- src/zip.ts
+++ src/zip.ts
@@ -2,10 +2,10 @@ import pako from 'pako'
 
 export class Zip {
   compress(data: Uint8Array | ArrayBuffer | string): Uint8Array {
-    return pako.deflate(data)
+    return pako.gzip(data)
   }
 
   decompress(buffer: Uint8Array | ArrayBuffer): string {
-    return pako.inflate(buffer, { to: 'string' })
+    return pako.ungzip(buffer, { to: 'string' })
   }
 }
