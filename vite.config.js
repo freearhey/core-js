08d01df0eacc30af40e46664bfba0e914a7a4e02
freearhey <7253922+freearhey@users.noreply.github.com>
2025-09-14 04:58:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { resolve } from 'node:path'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'dist/index.browser.js'),
+      entry: resolve(__dirname, 'dist/index.js'),
       name: '@freearhey/core',
-      fileName: 'index.browser'
+      fileName: 'index'
     },
     outDir: 'dist',
     emptyOutDir: false
