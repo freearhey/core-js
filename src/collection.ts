22ca1ed270ef1caae6daff6453d577d40e44f53a
freearhey <7253922+freearhey@users.noreply.github.com>
2025-09-16 12:07:15 +0300

    Update collection.ts

--- src/collection.ts
+++ src/collection.ts
@@ -2,16 +2,16 @@ import _ from 'lodash'
 import { orderBy, Order } from 'natural-orderby'
 import { Dictionary } from './dictionary.js'
 
-type Iteratee = (value: any, value2?: any) => void
+type Iteratee = (value: any) => void
 
-export class Collection {
-  _items: any[] = []
+export class Collection<Type> {
+  private _items: Type[] = []
 
-  constructor(items?: any) {
+  constructor(items?: Type[]) {
     this._items = Array.isArray(items) ? items : []
   }
 
-  first(iteratee?: Iteratee): any {
+  first(iteratee?: Iteratee): Type {
     if (iteratee) {
       return _.find(this._items, iteratee)
     }
@@ -19,7 +19,7 @@ export class Collection {
     return this._items[0]
   }
 
-  last(predicate?: Iteratee): any {
+  last(predicate?: Iteratee): Type {
     if (predicate) {
       return _.findLast(this._items, predicate)
     }
@@ -27,36 +27,36 @@ export class Collection {
     return this._items[this._items.length - 1]
   }
 
-  find(iteratee: Iteratee): any {
+  find(iteratee: Iteratee): Type {
     return _.find(this._items, iteratee)
   }
 
-  add(data: any): this {
+  add(data: Type): this {
     this._items.push(data)
 
     return this
   }
 
-  push(data: any): this {
+  push(data: Type): this {
     return this.add(data)
   }
 
-  intersects(collection: Collection): Collection {
+  intersects(collection: Collection<Type>): Collection<Type> {
     const items = _.intersection(this._items, collection.all())
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  intersectsBy(collection: Collection, iteratee: Iteratee): Collection {
+  intersectsBy(collection: Collection<Type>, iteratee: Iteratee): Collection<Type> {
     const items = _.intersectionBy(this._items, collection.all(), iteratee)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  slice(start?: number, end?: number): Collection {
+  slice(start?: number, end?: number): Collection<Type> {
     const items = _.slice(this._items, start, end)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
   count(): number {
@@ -67,44 +67,44 @@ export class Collection {
     return this._items.join(separator)
   }
 
-  indexOf(value: string | number | object): number {
-    return this._items.indexOf(value)
+  indexOf(instance: Type): number {
+    return this._items.indexOf(instance)
   }
 
-  uniq(): Collection {
+  uniq(): Collection<Type> {
     const items = _.uniq(this._items)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  filter(iteratee: Iteratee): Collection {
+  filter(iteratee: Iteratee): Collection<Type> {
     const items = _.filter(this._items, iteratee)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  forEach(callback: (item: any, index?: number) => void): this {
+  forEach(callback: (item: Type, index?: number) => void): this {
     this._items.forEach(callback)
 
     return this
   }
 
-  remove(iteratee: Iteratee): Collection {
+  remove(iteratee: Iteratee): Collection<Type> {
     const removed = _.remove(this._items, iteratee)
 
-    return new Collection(removed)
+    return new Collection<Type>(removed)
   }
 
-  concat(collection: Collection): Collection {
+  concat(collection: Collection<Type>): Collection<Type> {
     const items = this._items.concat(collection._items)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  reduce(iteratee: Iteratee): Collection {
+  reduce(iteratee: Iteratee): Collection<Type> {
     const items = this._items.reduce(iteratee, [])
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
   isEmpty(): boolean {
@@ -115,28 +115,28 @@ export class Collection {
     return this._items.length > 0
   }
 
-  sort(): Collection {
+  sort(): Collection<Type> {
     const items = this._items.sort()
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
-  sortBy(iteratees: Iteratee | Iteratee[]): Collection {
+  sortBy(iteratees: Iteratee | Iteratee[]): Collection<Type> {
     const items = _.sortBy(this._items, iteratees)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
   orderBy(
     iteratees: Iteratee | Iteratee[],
     orders: 'asc' | 'desc' | string[] = 'asc',
     natural: boolean = true
-  ): Collection {
+  ): Collection<Type> {
     const items = natural
       ? orderBy(this._items, iteratees, orders as Order)
       : _.orderBy(this._items, iteratees, orders as _.Many<boolean | 'asc' | 'desc'>)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
   keyBy(iteratee: Iteratee): Dictionary {
@@ -145,34 +145,18 @@ export class Collection {
     return new Dictionary(items)
   }
 
-  includes(value: any): boolean {
-    if (typeof value === 'function') {
-      const found = _.find(this._items, value)
-
-      return !!found
-    } else if (typeof value === 'string') {
-      return _.includes(this._items, value)
-    }
-
-    return false
+  includes(value: Iteratee): boolean {
+    return !!_.find(this._items, value)
   }
 
-  missing(value: any): boolean {
-    if (typeof value === 'function') {
-      const found = _.find(this._items, value)
-
-      return !found
-    } else if (typeof value === 'string') {
-      return !_.includes(this._items, value)
-    }
-
-    return true
+  missing(value: Iteratee): boolean {
+    return !_.find(this._items, value)
   }
 
-  uniqBy(iteratee: Iteratee): Collection {
+  uniqBy(iteratee: Iteratee): Collection<Type> {
     const items = _.uniqBy(this._items, iteratee)
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 
   groupBy(iteratee: Iteratee): Dictionary {
@@ -181,13 +165,13 @@ export class Collection {
     return new Dictionary(object)
   }
 
-  map(iteratee: Iteratee): Collection {
+  map(iteratee: Iteratee): Collection<Type> {
     const items = this._items.map(iteratee)
 
-    return new Collection(items)
+    return new Collection<Type>(items as Type[])
   }
 
-  all(): any[] {
+  all(): Type[] {
     return this._items
   }
 
@@ -195,7 +179,7 @@ export class Collection {
     return JSON.stringify(this._items)
   }
 
-  mergeBy(collection: Collection, iteratee: Iteratee): Collection {
+  mergeBy(collection: Collection<Type>, iteratee: Iteratee): Collection<Type> {
     this._items = _.unionBy(this._items, collection._items, iteratee)
 
     return this
@@ -204,6 +188,6 @@ export class Collection {
   clone() {
     const items = [...this._items]
 
-    return new Collection(items)
+    return new Collection<Type>(items)
   }
 }
