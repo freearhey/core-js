82cb44d1f25071de04376c3539c97bd8d8b29e3d
freearhey <7253922+freearhey@users.noreply.github.com>
2025-09-18 05:31:33 +0300

    Update tests

--- tests/collection.test.ts
+++ tests/collection.test.ts
@@ -3,91 +3,114 @@ import { describe, it, expect } from 'vitest'
 
 describe('collection', () => {
   it('can create empty collection', () => {
-    const collection = new Collection()
+    const collection = new Collection<string>()
 
     expect(collection.all()).toEqual([])
   })
 
   it('can return first item', () => {
-    const collection = new Collection(['a', 'b', 'c'])
+    const collection = new Collection<string>(['a', 'b', 'c'])
 
     expect(collection.first()).toBe('a')
   })
 
   it('can return last item', () => {
-    const collection = new Collection(['a', 'b', 'c'])
+    const collection = new Collection<string>(['a', 'b', 'c'])
 
     expect(collection.last()).toBe('c')
   })
 
   it('can find item', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
     expect(collection.find(i => i.name === 'b')).toEqual({ name: 'b' })
   })
 
   it('can add item', () => {
-    const collection = new Collection(['a'])
+    const collection = new Collection<string>(['a'])
     collection.add('b')
-    collection.push('c')
+    collection.add('c')
 
     expect(collection.all()).toEqual(['a', 'b', 'c'])
   })
 
   it('can find all intersections with another collection', () => {
-    const collectionA = new Collection(['a', 'b', 'c'])
-    const collectionB = new Collection(['b'])
-    const collectionC = new Collection(['c'])
+    const collectionA = new Collection<string>(['a', 'b', 'c'])
+    const collectionB = new Collection<string>(['b'])
+    const collectionC = new Collection<string>(['c'])
 
-    expect(collectionA.intersects(collectionB)).toEqual(new Collection(['b']))
-    expect(collectionB.intersects(collectionC)).toEqual(new Collection())
+    expect(collectionA.intersects(collectionB)).toEqual(new Collection<string>(['b']))
+    expect(collectionB.intersects(collectionC)).toEqual(new Collection<string>())
   })
 
   it('can find all intersections with another collection by key', () => {
-    const collectionA = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
-    const collectionB = new Collection([{ name: 'b' }])
-    const collectionC = new Collection([{ name: 'c' }])
+    const collectionA = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
+    const collectionB = new Collection<{ name: string }>([{ name: 'b' }])
+    const collectionC = new Collection<{ name: string }>([{ name: 'c' }])
 
     expect(collectionA.intersectsBy(collectionB, i => i.name)).toEqual(
-      new Collection([{ name: 'b' }])
+      new Collection<{ name: string }>([{ name: 'b' }])
     )
-    expect(collectionB.intersectsBy(collectionC, i => i.name)).toEqual(new Collection())
+    expect(collectionB.intersectsBy(collectionC, i => i.name)).toEqual(new Collection<string>())
   })
 
   it('can return count of items', () => {
-    const collection = new Collection(['a', 'b'])
+    const collection = new Collection<string>(['a', 'b'])
 
     expect(collection.count()).toEqual(2)
   })
 
+  it('can return random item', () => {
+    const collection = new Collection<string>(['a', 'b'])
+
+    expect(collection.sample()).toBeOneOf(['a', 'b'])
+  })
+
   it('can join items into string', () => {
-    const collection = new Collection(['a', 'b'])
+    const collection = new Collection<string>(['a', 'b'])
 
     expect(collection.join(',')).toEqual('a,b')
   })
 
   it('can return index of item', () => {
-    const collection = new Collection(['a', 'b'])
+    const collection = new Collection<string>(['a', 'b'])
 
     expect(collection.indexOf('b')).toEqual(1)
   })
 
   it('can remove duplicates', () => {
-    const collection = new Collection(['a', 'b', 'b', 'c'])
+    const collection = new Collection<string>(['a', 'b', 'b', 'c'])
 
-    expect(collection.uniq()).toEqual(new Collection(['a', 'b', 'c']))
+    expect(collection.uniq()).toEqual(new Collection<string>(['a', 'b', 'c']))
   })
 
   it('can filter items', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
     expect(collection.filter(i => i.name === 'b')).toEqual(
-      new Collection([{ name: 'b' }, { name: 'b' }])
+      new Collection<{ name: string }>([{ name: 'b' }, { name: 'b' }])
     )
   })
 
   it('can loop through the items', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
     let output = ''
     collection.forEach(item => {
@@ -97,84 +120,113 @@ describe('collection', () => {
       output += item.name + index
     })
     expect(output).toEqual('abca0b1c2')
-    expect(collection).toEqual(new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }]))
+    expect(collection).toEqual(
+      new Collection<{ name: string }>([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    )
   })
 
   it('can remove item by value', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
     const removed = collection.remove(i => i.name === 'b')
-    expect(removed).toEqual(new Collection([{ name: 'b' }]))
-    expect(collection).toEqual(new Collection([{ name: 'a' }, { name: 'c' }]))
+    expect(removed).toEqual(new Collection<{ name: string }>([{ name: 'b' }]))
+    expect(collection).toEqual(new Collection<{ name: string }>([{ name: 'a' }, { name: 'c' }]))
   })
 
   it('can concatinate with another collection', () => {
-    const collectionA = new Collection(['a'])
-    const collectionB = new Collection(['b'])
+    const collectionA = new Collection<string>(['a'])
+    const collectionB = new Collection<string>(['b'])
 
-    expect(collectionA.concat(collectionB)).toEqual(new Collection(['a', 'b']))
+    collectionA.concat(collectionB)
+
+    expect(collectionA).toEqual(new Collection<string>(['a', 'b']))
   })
 
   it('can check if collection is empty', () => {
-    const collectionA = new Collection(['a'])
-    const collectionB = new Collection([])
+    const collectionA = new Collection<string>(['a'])
+    const collectionB = new Collection<string>([])
 
     expect(collectionA.isEmpty()).toBe(false)
     expect(collectionB.isEmpty()).toBe(true)
   })
 
   it('can check if collection is not empty', () => {
-    const collectionA = new Collection(['a'])
-    const collectionB = new Collection([])
+    const collectionA = new Collection<string>(['a'])
+    const collectionB = new Collection<string>([])
 
-    expect(collectionA.notEmpty()).toBe(true)
-    expect(collectionB.notEmpty()).toBe(false)
+    expect(collectionA.isNotEmpty()).toBe(true)
+    expect(collectionB.isNotEmpty()).toBe(false)
   })
 
   it('can sort items', () => {
-    const collection = new Collection(['b', 'a', 'c'])
+    const collectionA = new Collection<string>(['b', 'a', 'c'])
+    const collectionB = new Collection<{ name: string }>([
+      { name: 'b' },
+      { name: 'a' },
+      { name: 'c' }
+    ])
 
-    expect(collection.sort()).toEqual(new Collection(['a', 'b', 'c']))
+    expect(collectionA.sort()).toEqual(new Collection<string>(['a', 'b', 'c']))
+    expect(collectionB.sort()).toEqual(
+      new Collection<{ name: string }>([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    )
   })
 
   it('can clone itself', () => {
-    const collection = new Collection(['b', 'a', 'c'])
+    const collection = new Collection<string>(['b', 'a', 'c'])
     const clone = collection.clone().sort()
 
     expect(clone.all()).not.toEqual(collection.all())
   })
 
   it('can order items by value', () => {
-    const collection = new Collection([{ name: 'c' }, { name: 'a' }, { name: 'b' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'c' },
+      { name: 'a' },
+      { name: 'b' }
+    ])
 
-    expect(collection.orderBy(i => i.name)).toEqual(
-      new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    expect(collection.sortBy(i => i.name)).toEqual(
+      new Collection<{ name: string }>([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
     )
   })
 
   it('can order items by value desc', () => {
-    const collection = new Collection([{ name: 'c' }, { name: 'a' }, { name: 'b' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'c' },
+      { name: 'a' },
+      { name: 'b' }
+    ])
 
-    expect(collection.orderBy(i => i.name, 'desc')).toEqual(
-      new Collection([{ name: 'c' }, { name: 'b' }, { name: 'a' }])
+    expect(collection.sortBy(i => i.name, 'desc')).toEqual(
+      new Collection<{ name: string }>([{ name: 'c' }, { name: 'b' }, { name: 'a' }])
     )
   })
 
   it('can sort items by value but not natural', () => {
-    const collection = new Collection([
+    const collection = new Collection<{ name: string }>([
       { name: '01' },
       { name: '002' },
       { name: '100tv' },
       { name: '100TV' }
     ])
 
-    expect(collection.orderBy(i => i.name.toLowerCase(), 'asc', false)).toEqual(
-      new Collection([{ name: '002' }, { name: '01' }, { name: '100tv' }, { name: '100TV' }])
+    expect(collection.sortBy(i => i.name.toLowerCase(), 'asc', false)).toEqual(
+      new Collection<{ name: string }>([
+        { name: '002' },
+        { name: '01' },
+        { name: '100tv' },
+        { name: '100TV' }
+      ])
     )
   })
 
   it('can sort items by value', () => {
-    const collection = new Collection([
+    const collection = new Collection<{ name: string }>([
       { name: '002' },
       { name: '01' },
       { name: '100tv' },
@@ -182,12 +234,21 @@ describe('collection', () => {
     ])
 
     expect(collection.sortBy(i => i.name.toLowerCase())).toEqual(
-      new Collection([{ name: '002' }, { name: '01' }, { name: '100tv' }, { name: '100TV' }])
+      new Collection<{ name: string }>([
+        { name: '002' },
+        { name: '01' },
+        { name: '100tv' },
+        { name: '100TV' }
+      ])
     )
   })
 
   it('can convert Collection to Dictionary', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
     expect(collection.keyBy(i => i.name)).toEqual(
       new Dictionary({ a: { name: 'a' }, b: { name: 'b' }, c: { name: 'c' } })
@@ -195,8 +256,12 @@ describe('collection', () => {
   })
 
   it('can check if Collection includes value', () => {
-    const collectionA = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
-    const collectionB = new Collection(['a', 'b', 'c'])
+    const collectionA = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
+    const collectionB = new Collection<string>(['a', 'b', 'c'])
 
     expect(collectionA.includes((i: any) => i.name === 'a')).toEqual(true)
     expect(collectionA.includes((i: any) => i.name === 'd')).toEqual(false)
@@ -205,8 +270,12 @@ describe('collection', () => {
   })
 
   it('can check if Collection missing value', () => {
-    const collectionA = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
-    const collectionB = new Collection(['a', 'b', 'c'])
+    const collectionA = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
+    const collectionB = new Collection<string>(['a', 'b', 'c'])
 
     expect(collectionA.missing((i: any) => i.name === 'a')).toEqual(false)
     expect(collectionA.missing((i: any) => i.name === 'd')).toEqual(true)
@@ -215,22 +284,26 @@ describe('collection', () => {
   })
 
   it('can remove duplicates by value', () => {
-    const collection = new Collection([{ value: 0.4 }, { value: 1.2 }, { value: 0.2 }])
+    const collection = new Collection<{ value: number }>([
+      { value: 0.4 },
+      { value: 1.2 },
+      { value: 0.2 }
+    ])
 
     expect(collection.uniqBy(i => Math.ceil(i.value))).toEqual(
-      new Collection([{ value: 0.4 }, { value: 1.2 }])
+      new Collection<{ value: number }>([{ value: 0.4 }, { value: 1.2 }])
     )
   })
 
   it('can group by value', () => {
-    const collection = new Collection([
+    const collection = new Collection<{ tag: string; name: string }>([
       { tag: 'a', name: 'a' },
       { tag: 'a', name: 'b' },
       { tag: 'b', name: 'c' }
     ])
 
     expect(collection.groupBy(i => i.tag)).toEqual(
-      new Dictionary({
+      new Dictionary<{ tag: string; name: string }[]>({
         a: [
           { tag: 'a', name: 'a' },
           { tag: 'a', name: 'b' }
@@ -241,61 +314,28 @@ describe('collection', () => {
   })
 
   it('can iterates through the collection', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' }
+    ])
 
-    expect(collection.map(i => i.name)).toEqual(new Collection(['a', 'b', 'c']))
+    expect(collection.map(i => i.name)).toEqual(new Collection<string>(['a', 'b', 'c']))
   })
 
   it('can return all items as array', () => {
-    const collection = new Collection(['a', 'b', 'c'])
+    const collection = new Collection<string>(['a', 'b', 'c'])
 
     expect(collection.all()).toEqual(['a', 'b', 'c'])
   })
 
   it('can convert collection to json string', () => {
-    const collection = new Collection([{ name: 'a' }, { name: 'b' }, { name: 'c' }])
-
-    expect(collection.toJSON()).toEqual('[{"name":"a"},{"name":"b"},{"name":"c"}]')
-  })
-
-  it('can merge with another collection', () => {
-    const collectionA = new Collection([
-      { id: 1, value: 'a', type: 'c' },
-      { id: 2, value: 'b' }
-    ])
-    const collectionB = new Collection([
-      { id: '1', value: 'c' },
-      { id: 3, value: 'd' }
-    ])
-
-    expect(collectionA.mergeBy(collectionB, item => item.id.toString()).toJSON()).toEqual(
-      JSON.stringify([
-        { id: 1, value: 'a', type: 'c' },
-        { id: 2, value: 'b' },
-        { id: 3, value: 'd' }
-      ])
-    )
-  })
-
-  it('can reduce items', () => {
-    const collection = new Collection([
-      { name: 'a', children: [{ name: 'aa' }, { name: 'ab' }] },
-      { name: 'b', children: [{ name: 'ba' }, { name: 'bb' }] },
+    const collection = new Collection<{ name: string }>([
+      { name: 'a' },
+      { name: 'b' },
       { name: 'c' }
     ])
 
-    expect(
-      collection
-        .reduce((output, item) => {
-          if (item.children) {
-            output = output.concat(item.children)
-          }
-
-          return output
-        })
-        .all()
-    ).toEqual(
-      new Collection([{ name: 'aa' }, { name: 'ab' }, { name: 'ba' }, { name: 'bb' }]).all()
-    )
+    expect(collection.toJSON()).toEqual('[{"name":"a"},{"name":"b"},{"name":"c"}]')
   })
 })
--- tests/template.test.ts
+++ tests/template.test.ts
@@ -1,15 +1,15 @@
 import { describe, it, expect } from 'vitest'
-import { StringTemplate } from '../src'
+import { Template } from '../src'
 
 describe('stringTemplate', () => {
   it('can return list of variables', () => {
-    const template = new StringTemplate('Hello {name}, you have {count} unread messages')
+    const template = new Template('Hello {name}, you have {count} unread messages')
 
     expect(template.variables()).toEqual(['name', 'count'])
   })
 
   it('can format template', () => {
-    const template = new StringTemplate('Hello {name}, you have {count} unread messages')
+    const template = new Template('Hello {name}, you have {count} unread messages')
 
     expect(template.format({ name: 'Bob', count: 10 })).toEqual(
       'Hello Bob, you have 10 unread messages'
